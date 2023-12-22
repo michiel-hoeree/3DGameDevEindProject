7ade7a8800907731eab778e6558dafaf3e286dd2
inge129116 <113992394+inge129116@users.noreply.github.com>
2023-12-22 14:29:43 +0100

    map inge gemaakt

---
+++ Assets/inge/scripts/boot_bewegen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boot_bewegen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movement_forward;
+    public float movement_left_right;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
