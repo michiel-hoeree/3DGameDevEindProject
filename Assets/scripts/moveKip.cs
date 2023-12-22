3ef08d0b1c2967c6eea32d381040e3e9d9c9b321
Eline Van Zeir <elinevanzeir@hotmail.com>
2023-12-22 14:46:31 +0100

    kip bewegen

--- Assets/scripts/moveKip.cs
+++ Assets/scripts/moveKip.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class moveKip : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+
+    public float speed = 5f;
+    public float fenceDistance = 3f;
+    public string fenceTag = "Fence";
+
+    private Vector3 direction;
+
     void Start()
     {
-        
+        direction = RandomDirection();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += direction * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, FindClosestFence().transform.position) < fenceDistance)
+        {
+            direction = RandomDirection();
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+    Vector3 RandomDirection()
+    {
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+        return randomDirection;
+    }
+
+    GameObject FindClosestFence()
+    {
+        GameObject[] fences = GameObject.FindGameObjectsWithTag(fenceTag);
+        GameObject closestFence = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject fence in fences)
+        {
+            float distance = Vector3.Distance(transform.position, fence.transform.position);
+            if (distance < closestDistance)
+            {
+                closestFence = fence;
+                closestDistance = distance;
+            }
+        }
+
+        return closestFence;
     }
 }
+
+
