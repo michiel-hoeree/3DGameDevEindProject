ea7fafb295df01ecad6eb61a15777ea19c05a4ea
Eline Van Zeir <elinevanzeir@hotmail.com>
2023-12-27 21:56:36 +0100

    eieren leggen obv teller

--- Assets/scripts/VoedselSpeler.cs
+++ Assets/scripts/VoedselSpeler.cs
@@ -8,12 +8,15 @@ using UnityEngine.UI;
 public class VoedselSpeler : MonoBehaviour
 {
     public TMP_Text voedselTellerText;
+    public static VoedselSpeler instance;
+
     private int voedselTeller = 0;
-    private int voedselInVoederbak = 0;
+    public int voedselInVoederbak = 0;
 
     void Start()
     {
         UpdateVoedselTellerText();
+        instance = this;
     }
 
     void UpdateVoedselTellerText()
@@ -37,5 +40,10 @@ public class VoedselSpeler : MonoBehaviour
             voedselTeller = 0;
             UpdateVoedselTellerText();
         }
+    }
+
+    public int GetVoedselTeller()
+    {
+        return voedselInVoederbak;
     }
 }
