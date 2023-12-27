ea44b25ccb281c376b4a460fc2cb2f65e2539836
Eline Van Zeir <elinevanzeir@hotmail.com>
2023-12-27 20:59:29 +0100

    voedsel raken en score zien

---
+++ Assets/scripts/VoedselSpeler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VoedselSpeler : MonoBehaviour
+{
+    public TMP_Text voedselTellerText;
+    private int voedselTeller = 0;
+
+    void Start()
+    {
+        UpdateVoedselTellerText();
+    }
+
+    void UpdateVoedselTellerText()
+    {
+        voedselTellerText.text = "Voedsel: " + voedselTeller;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Voedsel"))
+        {
+            // Het voedsel is geraakt door de speler
+            voedselTeller++;
+            UpdateVoedselTellerText();
+            Destroy(other.gameObject); // Verwijder het voedselobject
+        }
+        else if (other.CompareTag("Voederbak"))
+        {
+            // De voederbak is geraakt door de speler
+            voedselTeller = 0;
+            UpdateVoedselTellerText();
+        }
+    }
+}
